1e6cf9acb634643a4abdd1ff4fe0301f2bff7ed2
oxynger <oxyner@gmail.com>
2019-04-30 21:03:28 +0400

    remove excess code and dependency Удалил ненужный код, заодно по фиксил баг из за которого зависала программа.

--- Кодер/main.cpp
+++ Кодер/main.cpp
@@ -1,58 +1,15 @@
 #include <iostream>
-#include <locale>
 #include <string>
-#include <stdlib.h>
 #include <fstream>
-#include <cstring>
 #include <vector>
 #include <map>
-#include <stack>
 
 using namespace std;
 
-vector<string> alpf, alp_num, alp_pun, alp_all_alpf;
-
-void record_alpf()
-{
-    fstream in;
-    string str;
-    alpf.clear();
-    alp_num.clear();
-    alp_pun.clear();
-    in.open("alpf.txt", ios::in);
-    while (!in.eof())
-    {
-        getline(in, str);
-        alpf.push_back(str);
-    }
-    in.close();
-    in.open("num.txt", ios::in);
-    while (!in.eof())
-    {
-        getline(in, str);
-        alp_num.push_back(str);
-    }
-    in.close();
-    in.open("pun.txt", ios::in);
-    while (!in.eof())
-    {
-        getline(in, str);
-        alp_pun.push_back(str);
-    }
-    in.close();
-    in.open("all_alpf.txt", ios::in);
-    while (!in.eof())
-    {
-        getline(in, str);
-        alp_all_alpf.push_back(str);
-    }
-    in.close();
-}
-
-void clear_file()
+void clear_file(string filePath)
 {
     ofstream f;
-    f.open("out_tab.txt", ios::out | ios::trunc);
+    f.open(filePath, ios::out | ios::trunc);
     f.close();
 }
 
@@ -65,9 +22,8 @@ map<char, int> CreateASCIITable()
     return ASCII;
 }
 
-
 // Записываем таблицу хафмана в файл
-void WriteTable(vector<vector<int> > tableHuffman, string filePath)
+void WriteTable(vector<vector<int>> tableHuffman, string filePath)
 {
     ofstream out(filePath, ios_base::app);
     if (out.is_open())
@@ -80,17 +36,16 @@ void WriteTable(vector<vector<int> > tableHuffman, string filePath)
             }
             out << endl;
         }
-        
     }
 
     out.close();
 }
 
-vector<vector<int> > CreateTableHuffman(const string &input_str)
+vector<vector<int>> CreateTableHuffman(const string &input_str)
 {
     auto ASCII = CreateASCIITable();
 
-    vector<vector<int> > tableHuffman(255, vector<int>(255));
+    vector<vector<int>> tableHuffman(255, vector<int>(255));
 
     string str = input_str;
 
@@ -100,39 +55,28 @@ vector<vector<int> > CreateTableHuffman(const string &input_str)
     {
         int codeSecondLetter = ASCII[str.at(i)];
         cout << codeSecondLetter << endl;
-        
+
         tableHuffman[codeFirstLetter][codeSecondLetter]++;
         if (codeFirstLetter == 255 && codeSecondLetter == 2)
             cout << "HI-------------------" << endl;
 
         codeFirstLetter = codeSecondLetter;
     }
-    
+
     return tableHuffman;
-}
-
-void Coding()
-{
-}
-
-void DeCoding(string str)
-{
 }
 
 int main()
 {
+    string outFile = "out_tab.txt";
+
     setlocale(LC_ALL, "Russian");
 
-    char name_file[20] = "inp.txt";
-
-    CreateASCIITable();
-
-    cout << "Имя файла: " << name_file << endl;
-
-    clear_file();
+    string openedFile = "inp.txt";
+    cout << "Имя файла из которого берем входные данные: " << openedFile << endl;
 
     fstream infile;
-    infile.open(name_file, ios::in);
+    infile.open(openedFile, ios::in);
 
     string fullText;
     if (infile.is_open())
@@ -141,39 +85,17 @@ int main()
         while (getline(infile, line))
         {
             fullText.append(line);
-            Coding();
         }
     }
-    
+
     vector<vector<int> > HuffmanTable;
-    try
-    {    
-        HuffmanTable = CreateTableHuffman(fullText);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << "in CreateTableHuffman functon" << endl;
-        std::cerr << e.what() << '\n';
-    }
-        
+
+    HuffmanTable = CreateTableHuffman(fullText);
+
     infile.close();
-    
-    WriteTable(HuffmanTable, "out_tab.txt");
 
-    fstream infdec;
-    infdec.open("out.txt", ios::in);
-
-    if (!infdec.is_open())
-    {
-        while (!infdec.eof())
-        {
-            string str;
-            getline(infdec, str);
-            DeCoding(str);
-        }
-    }
-
-    infdec.close();
+    clear_file(outFile);
+    WriteTable(HuffmanTable, outFile);
 
     return 0;
 }
