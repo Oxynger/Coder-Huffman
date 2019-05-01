8342698c879bb5ed13a9dbb206953721a8686cec
oxynger <oxyner@gmail.com>
2019-05-01 20:17:56 +0400

    Prepare to mix algoritms Переписал алгоритм кодера так что бы он работал с map. Что бы в дальнейшем можно было без проблем работать с алгоритмом хффмана Ввел класс CountOfSymbols доплоняющий работу map.

--- Кодер/main.cpp
+++ Кодер/main.cpp
@@ -1,10 +1,81 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <map>
 
 using namespace std;
+
+class CountOfSymbols : public map<char, map<char, int>>
+{
+private:
+    uint encodingBites = 255;
+
+    string converServiceSymbols(char Symbol)
+    {
+        switch ((int)Symbol)
+        {
+        case 13:
+            return "Enter";
+        
+        case 32:
+            return "Space";
+        default:
+            return string(1,Symbol);
+        }
+        return "";
+    }
+
+public:
+    CountOfSymbols() : map()
+    {
+        // Заполняем количество символов нулями.
+        for (char line = 0; line < encodingBites + 1; line++)
+        {
+            for (char column = 0; column < encodingBites + 1; column++)
+            {
+                (*this)[line][column] = 0;
+            }
+        }
+    }
+    string FormatOutput()
+    {
+        stringstream OutputString;
+        for (auto LineCursor = this->begin(); LineCursor != this->end(); LineCursor++)
+        {
+            auto Line = LineCursor->second;
+            for (auto ColumnCursor = Line.begin(); ColumnCursor != Line.end(); ColumnCursor++)
+            {
+                auto Number = ColumnCursor->second;
+                auto FirstSymbol = converServiceSymbols(LineCursor->first);
+                auto SecondSymdol = converServiceSymbols(ColumnCursor->first);
+
+                if (ColumnCursor->second != 0)
+                {
+                    OutputString << "Symbol: " << FirstSymbol << " -> " << SecondSymdol << " - " << Number << endl;
+                }
+            }
+        }
+
+        return OutputString.str();
+    }
+
+    void WriteInFile(ofstream &out)
+    {
+        for (auto LineCursor = this->begin(); LineCursor != this->end(); LineCursor++)
+        {
+            auto Line = LineCursor->second;
+
+            for (auto Number = Line.begin(); Number != Line.end(); Number++)
+            {
+                out << Number->second << "  ";
+            }
+
+            out << endl;
+        }
+    }
+};
 
 void clear_file(string filePath)
 {
@@ -22,57 +93,53 @@ map<char, int> createASCIITable()
     return ASCII;
 }
 
+string ForamtCountOfSymbols(CountOfSymbols symbols)
+{
+}
+
 // Записываем таблицу хафмана в файл
-void WriteTable(vector<vector<int>> tableHuffman, string filePath)
+void WriteTable(CountOfSymbols symbolsCount, string filePath)
 {
     ofstream out(filePath, ios_base::app);
+
     if (out.is_open())
     {
-        for (auto Line : tableHuffman)
-        {
-            for (auto Value : Line)
-            {
-                out << Value << " ";
-            }
-            out << endl;
-        }
+        cout << symbolsCount.FormatOutput() << endl;
+        symbolsCount.WriteInFile(out);
     }
 
     out.close();
 }
 
-vector<vector<int>> CreateTableHuffman(const string &input_str)
+CountOfSymbols CountingSymbols(const string &input_str)
 {
-    // Количество бит в кодеровки (ASCII - 255)
-    size_t encryptBitCount = 255;
     auto ASCII = createASCIITable();
 
-    vector<vector<int>> tableHuffman(encryptBitCount, vector<int>(encryptBitCount));
+    CountOfSymbols countOfSymbols;
 
     string str = input_str;
 
-    int codeFirstLetter = ASCII[str.at(0)];
-    cout << codeFirstLetter << endl;
+    char FirstLetter = str.at(0);
+
     for (int i = 1; i < str.length(); i++)
     {
-        int codeSecondLetter = ASCII[str.at(i)];
-        cout << codeSecondLetter << endl;
+        char SecondLetter = str.at(i);
 
-        tableHuffman[codeFirstLetter][codeSecondLetter]++;
+        countOfSymbols[FirstLetter][SecondLetter]++;
 
-        codeFirstLetter = codeSecondLetter;
+        FirstLetter = SecondLetter;
     }
 
-    return tableHuffman;
+    return countOfSymbols;
 }
 
 int main()
 {
     string outFile = "out_tab.txt";
+    string openedFile = "inp.txt";
 
     setlocale(LC_ALL, "Russian");
 
-    string openedFile = "inp.txt";
     cout << "Имя файла из которого берем входные данные: " << openedFile << endl;
 
     fstream infile;
@@ -84,18 +151,17 @@ int main()
         string line;
         while (getline(infile, line))
         {
+            // Проверить что будет закодирован символ переноса строки
             fullText.append(line);
         }
     }
 
-    vector<vector<int>> HuffmanTable;
-
-    HuffmanTable = CreateTableHuffman(fullText);
+    auto numberOfSymbols = CountingSymbols(fullText);
 
     infile.close();
 
     clear_file(outFile);
-    WriteTable(HuffmanTable, outFile);
+    WriteTable(numberOfSymbols, outFile);
 
     return 0;
 }
