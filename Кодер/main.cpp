0c726a64894851865e78211d30a6e4dec98874a5
oxynger <oxyner@gmail.com>
2019-05-11 00:28:35 +0400

    add becnhmark for Coder

--- Кодер/main.cpp
+++ Кодер/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <chrono>
 #include "./CountSymbol/CountSymbols.hpp"
 #include "./CoderTable/CoderTable.hpp"
 
@@ -25,12 +26,30 @@ map<char, int> createASCIITable()
     return ASCII;
 }
 
+string getTextFromFile(std::fstream &in)
+{
+    std::string text;
+    if (in.is_open())
+    {
+        string line;
+        while (getline(in, line))
+        {
+            // Проверить что будет закодирован символ переноса строки
+            text.append(line);
+        }
+    }
+    in.close();
+
+    return text;
+}
+
 int main()
 {
-    string outTable = "out_tab.txt";
-    string codes = "out_code.txt";
-    string openedFile = "inp.txt";
-    string outDecrypt = "out_msg.txt";
+    const string outTable = "bin/data/out_tab.txt";
+    const string codes = "bin/data/out_code.txt";
+    const string openedFile = "inp.txt";
+    const string outDecrypt = "bin/data/out_msg.txt";
+    const string benchFile = "bin/data/coder_bench.txt";
 
     setlocale(LC_ALL, "Russian");
 
@@ -38,31 +57,31 @@ int main()
 
     fstream infile;
     infile.open(openedFile, ios::in);
+    std::ofstream benchOut(benchFile, ios::out);
+    
+    auto fullText = getTextFromFile(infile);
 
-    string fullText;
-    if (infile.is_open())
-    {
-        string line;
-        while (getline(infile, line))
-        {
-            // Проверить что будет закодирован символ переноса строки
-            fullText.append(line);
-        }
-    }
+    clear_file(outTable);
+    clear_file(codes);
+    clear_file(outDecrypt);
+    clear_file(benchFile);
+
+    // Начало отчета времени.
+    auto start = std::chrono::high_resolution_clock::now();
 
     CountSymbols numberOfSymbols(fullText);
 
     CoderTable HuffmanTable(numberOfSymbols, fullText);
 
-    infile.close();
-
-    clear_file(outTable);
-    clear_file(codes);
-    clear_file(outDecrypt);
-
     HuffmanTable.WriteTable(outTable);
     HuffmanTable.Encrypt(codes);
     HuffmanTable.Decrypt(outDecrypt, codes);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto elapsedMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+
+    benchOut << "milliseconds: " << elapsedMilliseconds << endl;
 
     return 0;
 }
